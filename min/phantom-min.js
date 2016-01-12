9cd27d0fae76b17a2a651352c60e4d7654c77d38
Xavier Alexandre <cuocoli@gmail.com>
2016-01-12 22:07:46 +0100

    Remove some margin left

--- min/phantom-min.js
+++ min/phantom-min.js
@@ -1,1 +1,1 @@
-var page=require("webpage").create();page.open("http://localhost:5757/map.html",function(){page.clipRect={top:1,left:1,width:800,height:800},window.setTimeout(function(){page.render("map_preview.png"),phantom.exit()},2e3)});
+var page=require("webpage").create();page.open("http://localhost:5757/map.html",function(){page.clipRect={top:2,left:2,width:800,height:800},window.setTimeout(function(){page.render("map_preview.png"),phantom.exit()},2e3)});
